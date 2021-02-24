a3ffc85b3d2f9cc69b5bb0e8aed9f56e9eef6b94
imelchenko61@gmail.com <imelchenko61@gmail.com>
2021-02-24 23:20:17 +0100

    Local state

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from '../../Common/Preloader/Preloader';
 import M from './ProfileInfo.module.css';
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -7,18 +8,10 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <img className={M.content__img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCosNE8U8r4l6IY0NHJ7icaHynM8gFLQimNg&usqp=CAU" />
+            {/* <img className={M.content__img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCosNE8U8r4l6IY0NHJ7icaHynM8gFLQimNg&usqp=CAU" /> */}
             <div className={M.MainPost}>
                 <img id={M.img} src={props.profile.photos.small} />
-                <div className={M.data}>
-                    <h3>Dmitry K.</h3>
-                    <p>
-                        Date of Birth:2january<br />
-                                City: Minsk<br />
-                                Edication:BSU'11<br />
-                                Web Site:adsdasdad<br />
-                    </p>
-                </div>
+                <ProfileStatus status = "Na Na Banana"/>
             </div>
         </div>
 
