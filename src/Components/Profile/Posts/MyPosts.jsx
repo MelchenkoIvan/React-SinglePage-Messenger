a5d1a970556a77523a36d505c8249aedc0355e9a
imelchenko61@gmail.com <imelchenko61@gmail.com>
2021-03-02 21:50:45 +0100

    memo

--- src/Components/Profile/Posts/MyPosts.jsx
+++ src/Components/Profile/Posts/MyPosts.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import { render } from '@testing-library/react';
+import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form';
-import { requiredField,maxLengthCreator } from '../../../utils/Validators/Validators';
-import {Textarea} from '../../Common/FormsControls/FormsControls';
+import { requiredField, maxLengthCreator } from '../../../utils/Validators/Validators';
+import { Textarea } from '../../Common/FormsControls/FormsControls';
 import M from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = (props) => {
+const MyPosts = React.memo(props => {
+    //говорим когда прерисовать а когда нет 
+    //либо можем унаследовать PureComponent и результат тот же
+    // shouldComponentUpdate(nextProps, nextState){
+    //     return nextProps != this.props || nextState != this.state;
+    // }
 
+    //можем использовать memo если не хотим класовую компоненту
     let postsElements =
         props.post.map(post => <Post message={post.text} countLike={post.countLike} />);
-   
+
     let onAddPost = (values) => {
         props.addPost(values.newPostText);
-    }
-
+    };
     return (
         <div className={M.posts}>
             <h3 className={M.titlePosts}>My Posts</h3>
-            <AddNewPostFormRedux onSubmit={onAddPost}/>
+            <AddNewPostFormRedux onSubmit={onAddPost} />
             <div className={M.posts}>
                 {postsElements}
             </div>
         </div>)
-}
+})
 
 const maxLength10 = maxLengthCreator(10);
 const AddNewPostForm = (props) => {
@@ -30,8 +36,8 @@ const AddNewPostForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit /* handleSubmit собирает данные из Field*/}>
             <div>
-                <Field component={Textarea} name={"newPostText"} placeholder="Enter your post" 
-                validate= {[requiredField,maxLength10]}/>
+                <Field component={Textarea} name={"newPostText"} placeholder="Enter your post"
+                    validate={[requiredField, maxLength10]} />
             </div>
             <div>
                 <button >Add post</button>
@@ -40,6 +46,6 @@ const AddNewPostForm = (props) => {
         </form>
     )
 }
-const AddNewPostFormRedux = reduxForm({form:"NewPostFormRedux"})(AddNewPostForm)
+const AddNewPostFormRedux = reduxForm({ form: "NewPostFormRedux" })(AddNewPostForm)
 
 export default MyPosts;
